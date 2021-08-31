3d036a368de05a9bb407dc68f6ec73f20c67b5e5
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-08-31 14:48:26 -0700

    Started Tester

--- src/ATM.java
+++ src/ATM.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
 import java.util.Scanner;
-public class ATM {
-private static HashMap<String, Double> ids = new HashMap();
-public static void deposit(String id, double amount)
+
+public class ATM
 {
+	private static HashMap<String, Double> ids = new HashMap();
+	
+	public static void deposit(String id, double amount)
+	{
 		ids.putIfAbsent(id, 0.0);
 		ids.replace(id, ids.get(id) + amount);
-}
+	}
 }
