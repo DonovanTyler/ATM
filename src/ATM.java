1a0a3d323010eb3cd046b3d39c1b4640a0270dec
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-08-30 22:51:30 -0700

    Code with completed tester and hashmap error

--- src/ATM.java
+++ src/ATM.java
@@ -4,7 +4,7 @@ public class ATM {
 private static HashMap<String, Double> ids;
 public static void deposit(String id, double amount)
 {
-	if(!ids.containsKey(id))
+	if(ids.isEmpty() || !ids.containsKey(id))
 	{
 		ids.put(id, amount);
 	}
@@ -12,6 +12,17 @@ public static void deposit(String id, double amount)
 	{
 		ids.put(id, ids.get(id) + amount);
 	}
+}
+private static boolean allNumbers(String check)
+{
+	for(int i = 0; i < check.length(); i++)
+	{
+		if(check.charAt(i) != '.' && check.charAt(i) != '0' && check.charAt(i) != '1' && check.charAt(i) != '2' && check.charAt(i) != '3' && check.charAt(i) != '4' && check.charAt(i) !='5' && check.charAt(i) != '6' && check.charAt(i) != '7' && check.charAt(i) != '8' && check.charAt(i) != '9')
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 public static void main (String [] args)
@@ -21,12 +32,13 @@ public static void main (String [] args)
 	System.out.println("Enter your account id: ");
 	String accountId = keyboard.next();
 	System.out.println("Enter the amount you'd like to deposit: ");
-	int deposit = Integer.parseInt(keyboardTwo.next());
-	while (deposit < 0)
+	String depositString = keyboardTwo.next();
+	while(!allNumbers(depositString) || Double.parseDouble(depositString) < 0)
 	{
-		System.out.println("Not a valid amount.\nEnter a new amount: ");
-	deposit = Integer.parseInt(keyboardTwo.next());
+		System.out.println ("The deposited amount must only be in positive integers\nEnter the amount you'd like to deposit: ");
+		depositString = keyboardTwo.next();
 	}
+	double deposit = Double.parseDouble(depositString);
 	ATM.deposit(accountId, deposit);
 	keyboard.close();
 	keyboardTwo.close();
