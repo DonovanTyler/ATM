1bad0a97f94647e12c9641e964ce0a78d1a9d449
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-08-30 21:56:01 -0700

    Code with tester Ver 1.0

--- src/ATM.java
+++ src/ATM.java
@@ -1,7 +1,8 @@
 import java.util.HashMap;
+import java.util.Scanner;
 public class ATM {
-	private HashMap<String, Double> ids;
-public void deposit(String id, double amount)
+private static HashMap<String, Double> ids;
+public static void deposit(String id, double amount)
 {
 	if(!ids.containsKey(id))
 	{
@@ -11,5 +12,23 @@ public void deposit(String id, double amount)
 	{
 		ids.put(id, ids.get(id) + amount);
 	}
+}
+
+public static void main (String [] args)
+{
+	Scanner keyboard = new Scanner (System.in);
+	Scanner keyboardTwo = new Scanner (System.in);
+	System.out.println("Enter your account id: ");
+	String accountId = keyboard.next();
+	System.out.println("Enter the amount you'd like to deposit: ");
+	int deposit = Integer.parseInt(keyboardTwo.next());
+	while (deposit < 0)
+	{
+		System.out.println("Not a valid amount.\nEnter a new amount: ");
+	deposit = Integer.parseInt(keyboardTwo.next());
+	}
+	ATM.deposit(accountId, deposit);
+	keyboard.close();
+	keyboardTwo.close();
 }
 }
