452d080bd00af5d62b7b78d6f5de24973d3c5fc6
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-08-30 21:36:08 -0700

    Added in actual code this time

--- src/ATM.java
+++ src/ATM.java
@@ -1,12 +1,15 @@
 import java.util.HashMap;
 public class ATM {
-	private HashMap<String, Integer> ids;
+	private HashMap<String, Double> ids;
 public void deposit(String id, double amount)
 {
 	if(!ids.containsKey(id))
 	{
-		
+		ids.put(id, amount);
 	}
-	
+	else
+	{
+		ids.put(id, ids.get(id) + amount);
+	}
 }
 }
