40a57c17ec36cfd28750b48c5424d2a37cb68895
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-08-31 14:54:11 -0700

    Updated tester with necessary methods

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,17 @@
-
-public class Tester {
-
+public class Tester
+{
+	public static void main (String [] args)
+	{
+		ATM atm = new ATM ();
+		atm.deposit("Winfrey", 25159684372.43);
+		atm.deposit("Bezos", 193495330293.43);
+		atm.deposit("Bezos", 20394353.35);
+		atm.withdraw("Bezos", 3593293.66);
+		atm.deposit("MrTheiss",64033.55);
+		atm.withdraw("MrTheiss", 443964.46);
+		atm.checkBalance("Bezos");
+		atm.checkBalance("Winfrey");
+		atm.checkBalance("YoMama");
+		atm.checkBalance("MrTheiss");
+	}
 }
